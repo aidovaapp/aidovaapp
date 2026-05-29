69409a94d307552357c980b68362e1dc0e95cc9b
aidovaapp <aidovaapp@gmail.com>
2026-05-29 09:42:57 +0100

    Create create-portal.js

---
+++ api/api/create-portal.js
@@ -0,0 +1,24 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+module.exports = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  try {
+    const { customerId } = req.body;
+
+    const session = await stripe.billingPortal.sessions.create({
+      customer: customerId,
+      return_url: 'https://aidova.app/'
+    });
+
+    return res.status(200).json({ url: session.url });
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+};
